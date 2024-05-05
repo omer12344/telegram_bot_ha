3457ddc8a249d2dffce01c835665f3f7cebc4bbf
omer12344 <130368849+omer12344@users.noreply.github.com>
2024-05-05 17:43:20 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,39 +1,42 @@
 import unittest
-from unittest.mock import MagicMock
-from main import handle_photo, handle_error
+from telegram import Bot
 
 
 class TestTelegramBot(unittest.TestCase):
-
     def setUp(self):
-        self.update = MagicMock()
-        self.context = MagicMock()
-        self.update.message = MagicMock()
+        self.bot_token = '6992973033:AAFLnQjfwWPFYKEJaB85zHI8y4-3rHPz7Ew'
+        self.bot = Bot(token=self.bot_token)
+        self.chat_id = '-4274807511'
 
-    def test_handle_photo_with_jpeg(self):
-        self.update.message.photo = [MagicMock()]
-        self.update.message.photo[-1].get_file.return_value.download.return_value =\
-            'C:\\Users\\MASTER\\PycharmProjects\\telegram_bot\\test_files\\bridge.jpg'
-        handle_photo(self.update, self.context)
-        (self.update.message.reply_text.assert_called_with
-         ("The SHA-256 hash of the image is: 9415c95bc5cabfebcc01a559dfb9fc3ecd1323faca9eb4416b3373bd0470afb4"))
+    def send_image_and_check_response(self, file_path, expected_response):
+        with open(file_path, 'rb') as image:
+            message = self.bot.send_photo(chat_id=self.chat_id, photo=image)
+        updates = self.bot.get_updates(offset=message.message_id)
+        if updates:
+            response = updates[-1].message.text
+            self.assertIn(expected_response, response)
 
-    def test_handle_photo_with_non_jpeg(self):
-        self.update.message.photo = [MagicMock()]
-        self.update.message.photo[-1].get_file.return_value.file_path =\
-            'C:\\Users\\MASTER\\PycharmProjects\\telegram_bot\\test_files\\pixelatedcat.png'
-        handle_photo(self.update, self.context)
-        self.update.message.reply_text.assert_called_with("Error: Please send a JPEG image file only.")
+    def test_send_jpeg_image(self):
+        self.send_image_and_check_response(
+            'C:\\Users\\MASTER\\PycharmProjects\\telegram_bot\\test_files\\bridge.jpg',
+            'SHA-256 hash of the image is:')
 
-    def test_handle_text_instead_of_image(self):
-        # Mocking the message type to trigger handle_error or similar function for non-document/text scenarios
-        self.update.message.text = 'This is a text, not an image.'
-        handle_error(self.update, self.context)
-        self.update.message.reply_text.assert_called_with("Error: Please send a JPEG image file only.")
+    def test_send_text_instead_of_image(self):
+        message = self.bot.send_message(chat_id=self.chat_id, text="This is a text message.")
+        updates = self.bot.get_updates(offset=message.message_id)
+        if updates:
+            response = updates[-1].message.text
+            self.assertIn("Error: Please send a JPEG image file only.", response)
+
+    def test_send_non_jpeg_image(self):
+        self.send_image_and_check_response(
+            'C:\\Users\\MASTER\\PycharmProjects\\telegram_bot\\test_files\\pixelatedcat.png',
+            "Error: Please send a JPEG image file only.")
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
 
 
